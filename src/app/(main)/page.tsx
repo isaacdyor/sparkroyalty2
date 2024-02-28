5252b023017b4e065be2368090a74d8fa8e9b351
Isaac Dyor <isaac@dyor.com>
2024-02-27 23:59:37 -0800

    works

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -4,7 +4,7 @@ export default async function AuthButton() {
   return (
     <div className="flex h-screen w-screen items-center justify-center gap-12 ">
       <Image
-        src="/logo.png"
+        src="https://raw.githubusercontent.com/isaacdyor/sparkroyalty2/main/public/logo.png"
         width={125.2}
         height={91.6}
         alt="Picture of the author"
