4bfc42f1b5ae5c0f06e58d9a6290f72998fd9904
Isaac Dyor <isaac@dyor.com>
2024-03-02 12:00:12 -0800

    start

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,16 +1,3 @@
-import Image from "next/image";
-import logo from "../../../public/logo.png";
-
-export default async function AuthButton() {
-  return (
-    <div className="flex h-screen w-screen items-center justify-center gap-12 ">
-      <Image
-        src={logo}
-        width={125.2}
-        height={91.6}
-        alt="Picture of the author"
-      />
-      <p className="text-9xl font-semibold">Spark royalty</p>
-    </div>
-  );
+export default async function HomePage() {
+  <p>Hello</p>;
 }
