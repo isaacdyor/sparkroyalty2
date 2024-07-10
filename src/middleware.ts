86b9dfb3720220c989cc20b284f679e661718af1
Isaac Dyor <isaac@dyor.com>
2024-07-10 15:59:57 -0700

    fixing sharp

--- src/middleware.ts
+++ src/middleware.ts
@@ -15,5 +15,5 @@ export async function middleware(request: NextRequest) {
 
 export const config = {
   matcher:
-    "/((?!api|_next/static|_next/image|favicon.ico|$|login|signup|auth).*)",
+    "/((?!api|_next/static|_next/image|favicon.ico|$|login|signup|auth|trial).*)",
 };
