edf567a1a6a33420e1c77e6699374beae46702b0
Isaac Dyor <isaac@dyor.com>
2024-07-10 16:26:20 -0700

    fixing homepage button

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,10 +1,10 @@
-"use client";
-
 import { Button } from "@/components/ui/button";
+import { api } from "@/trpc/server";
 
 import Link from "next/link";
 
-export default function HomePage() {
+export default async function HomePage() {
+  const user = await api.users.getCurrent.query();
   return (
     <div className="flex flex-col items-center justify-center gap-10 pt-40">
       <h1 className="text-center text-7xl font-semibold">
@@ -13,9 +13,21 @@ export default function HomePage() {
       <p className="text-center text-3xl font-semibold text-muted-foreground">
         The World&apos;s First Royalty Based Freelancing Marketplace
       </p>
-      <Button>
-        <Link href="/signup">Get Started</Link>
-      </Button>
+      {user?.active === "NONE" && (
+        <Button>
+          <Link href="/signup">Get Started</Link>
+        </Button>
+      )}
+      {user?.active === "INVESTOR" && (
+        <Button>
+          <Link href="/find-ventures">Get Started</Link>
+        </Button>
+      )}
+      {user?.active === "FOUNDER" && (
+        <Button>
+          <Link href="/venture/new">Get Started</Link>
+        </Button>
+      )}
 
       <div className="absolute top-20 -z-10 h-full max-h-[36rem] w-full max-w-5xl rounded-full bg-primary/10 blur-3xl" />
     </div>
